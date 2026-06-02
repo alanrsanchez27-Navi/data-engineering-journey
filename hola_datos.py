82a0a9232d6c8ee6754818a0571fe52b86f8a11d
alansranches27-Navi <alansanchez27@gmail.com>
2026-06-01 23:28:12 -0300

    fase 0 completa

---
+++ hola_datos.py
@@ -0,0 +1,6 @@
+nombre = "Data Engineer"    
+tecnologias = ["python","SQL","Azure ADF","Databricks","Power Bi"]
+print (f"hola, futuro {nombre}!")
+print (f"Este stack tiene {len(tecnologias)} tecnologias wazaaaaaaa")
+for coso, tecno in enumerate (tecnologias, 1):
+    print (f"{coso}. {tecno}")
